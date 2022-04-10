0aae27cf149a573d7ef63543273ac0e790e76778
rseewaldt <rseewaldt@parsionate.com>
2022-04-10 12:16:44 +0200

    created repo & Hello World

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HelloWorld
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("Hallo, Welt!");
+            Console.WriteLine("Hallo, Rolf!");
+            Console.ReadKey();
+
+            
+        }
+    }
+
+}
