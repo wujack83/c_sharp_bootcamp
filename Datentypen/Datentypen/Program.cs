64c081907650a397180edd539bb0ab225743c341
rseewaldt <rseewaldt@parsionate.com>
2022-04-10 22:49:12 +0200

    added Datentypen

---
+++ Datentypen/Datentypen/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        // Einstiegspunkt für das Programm
+        static void Main(string[] args)
+        {   
+            // Deklarieren und initialisieren der Variable Wert 1
+            int wert1 = 25;
+            
+            Console.WriteLine("Hallo, Welt!");
+            
+        }
+    }
+
+}
