0d650fbb000fe8d48cadf81fc02b130aa8d58456
rseewaldt <rseewaldt@parsionate.com>
2022-04-10 23:24:27 +0200

    Added Strings

--- Datentypen/Datentypen/Program.cs
+++ Datentypen/Datentypen/Program.cs
@@ -9,8 +9,26 @@ namespace HelloWorld
         {   
             // Deklarieren und initialisieren der Variable Wert 1
             int wert1 = 25;
+            // Deklarieren
+            int wert2;
+            //Initialisieren
+            wert2 = 15;
+
+            // Deklarieren mehrerer Variablen in einer Zeile
+            int wert3, wert4, wert5;
             
-            Console.WriteLine("Hallo, Welt!");
+            int summe = wert1 + wert2;
+
+            double d1 = 3.5;
+            double d2 = 1.337;
+            double ergebnis = d1 / d2;
+
+            // Verwendung von float
+            float f1 = 1.337f;
+            
+            Console.WriteLine("Die Summe ist: " + summe);
+            Console.WriteLine("Der Quotient ist: " + ergebnis);
+            Console.ReadKey();
             
         }
     }
---
+++ Strings/Strings/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Deklarieren und Initialisieren der Variable meinName
+            string meinName = "Rolf";
+            string nachricht = "Mein Name ist " + meinName; 
+            Console.WriteLine(nachricht);
+            Console.ReadKey();
+        }
+    }
+
+}
