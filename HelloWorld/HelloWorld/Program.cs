8b1af0f5593e52507aa63714bce0e32d9e268581
rseewaldt <rseewaldt@parsionate.com>
2022-04-10 12:20:39 +0200

    finished HelloWorld

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -1,17 +1,26 @@
-﻿using System;
+﻿/*
+ * 
+ * Mein 1. C# Programm für mein Training 
+ * Author: Rolf Seewaldt
+ * 
+ */
+using System;
 
 namespace HelloWorld
 { 
     class Program
     {
+        // Einstiegspunkt für das Programm
         static void Main(string[] args)
         {
+            //Ändere die Konsolenfarbe und gebe Hallo, Welt! aus
             Console.BackgroundColor = ConsoleColor.Blue;
             Console.ForegroundColor = ConsoleColor.Black;
             Console.WriteLine("Hallo, Welt!");
+            // Übung: Ändere den Code in Hallo, Rolf
             Console.WriteLine("Hallo, Rolf!");
-            Console.ReadKey();
-
+            // ReadKey lässt die Konsole offen
+            Console.ReadKey();           
             
         }
     }
