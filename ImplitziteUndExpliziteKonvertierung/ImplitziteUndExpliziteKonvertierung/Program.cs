70977277380eee6c43d8ffdcca802e9ca3b12b87
rseewaldt <rseewaldt@parsionate.com>
2022-04-11 08:47:18 +0200

    added KOnvertrierung

---
+++ ImplitziteUndExpliziteKonvertierung/ImplitziteUndExpliziteKonvertierung/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ImplitziteUndExpliziteKonvertierung
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Implizte Konvertierung
+            int num = 123453233;
+            long bigNum = num;
+
+            float myfloat = 13.37f;
+            double myDouble = myfloat;
+
+            //Explizite Konvertierung
+            int myInt;
+            double myNewDouble = 13.37;
+            myInt = (int)myNewDouble;
+
+            string myString = myNewDouble.ToString();
+            string myString2 = myString + "12";
+
+            bool sunIsShining = true;
+            string myBoolString = sunIsShining.ToString();
+
+            Console.WriteLine(myBoolString);
+            Console.ReadKey();
+        }
+    }
+}
