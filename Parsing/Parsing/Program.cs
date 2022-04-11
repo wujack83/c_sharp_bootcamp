904a584f552d652367476937a2699c6919fc819a
rseewaldt <rseewaldt@parsionate.com>
2022-04-11 08:57:31 +0200

    Added Parsing

---
+++ Parsing/Parsing/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Parsing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString = "15";
+            string mySecondString = "13";
+
+            int num1 = int.Parse(myString);
+            int num2 = int.Parse(mySecondString); 
+
+            int Result = num1 + num2;
+
+            Console.WriteLine(Result);
+            Console.ReadKey();
+        }
+    }
+}
