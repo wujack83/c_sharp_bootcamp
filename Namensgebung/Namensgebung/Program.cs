ea8b646afe179ad7f5b89dd46660137fcfadb76d
rseewaldt <rseewaldt@parsionate.com>
2022-04-11 08:21:00 +0200

    Added Namensgebung

---
+++ Namensgebung/Namensgebung/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Namensgebung
+{
+    // Klassennamen wie z.B. ClientActivity
+    internal class Program
+    {
+        // Methodennamen sind groß geschrieben und Verben z.B. CalculateValue
+        // Argumente von Methoden werden klein geschrieben
+        static void Main(string[] args)
+        {
+
+            // variable heßt wie z.B. itemCount
+            // nutze userControl statt usrCtr
+            // keine Zahlen am Anfang von Variablen
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- Strings/Strings/Program.cs
+++ Strings/Strings/Program.cs
@@ -8,8 +8,15 @@ namespace HelloWorld
         {
             //Deklarieren und Initialisieren der Variable meinName
             string meinName = "Rolf";
-            string nachricht = "Mein Name ist " + meinName; 
+            string nachricht = "Mein Name ist " + meinName;
+            int laenge = nachricht.Length;
+            // ToUpper macht alle Elemente des Strings groß
+            string allesGross = nachricht.ToUpper();
+            string allesklein = nachricht.ToLower();
             Console.WriteLine(nachricht);
+            Console.WriteLine(nachricht + " ist {0} Zeichen lang", laenge);
+            Console.WriteLine(allesGross);
+            Console.WriteLine(allesklein);
             Console.ReadKey();
         }
     }
